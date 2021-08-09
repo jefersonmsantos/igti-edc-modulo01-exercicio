7385b232f45d3d3387a1e7778ab6e45b107bcb02
Jeferson Machado Santos <jefersonmsantos@outlook.com>
2021-08-09 10:21:58 -0300

    adjust lambda .tf

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "executa_emr" {
     filename = "lambda_function.payload.zip"
     function_name = var.lambda_function_name
-    role = aws_iam_role.lamda.arn 
+    role = aws_iam_role.lambda.arn 
     handler = "lambda_function.handler"
     memory_size = 128
     timeout = 30
