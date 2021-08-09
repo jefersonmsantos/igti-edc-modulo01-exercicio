db556ee799bd5fc5e7860de080511216a56d9541
Jeferson Machado Santos <jefersonmsantos@outlook.com>
2021-08-09 09:09:37 -0300

    create lambda role / policy / bash

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "executa_emr" {
     function_name = var.lambda_function_name
     role = aws_iam_role.lamda.arn 
     handler = "lambda_function.handler"
-    memory_size = 128jefer
+    memory_size = 128
     timeout = 30
 
     source_code_hash = filebase64sha256("lambda_function.payload.zip")
