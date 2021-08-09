51e7c6bbb518edf31336b4051e650fb3570c0c24
Jeferson Machado Santos <jefersonmsantos@outlook.com>
2021-08-09 15:34:30 -0300

    add kinesis and glue crawler

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -16,3 +16,21 @@ resource "aws_s3_bucket" "datalake" {
         CURSO = "EDC"
     }
 }
+
+resource "aws_s3_bucket" "stream" {
+  bucket = "datalake-igti-streaming-bucket"
+  acl    = "private"
+
+  tags = {
+    IES   = "IGTI",
+    CURSO = "EDC"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
