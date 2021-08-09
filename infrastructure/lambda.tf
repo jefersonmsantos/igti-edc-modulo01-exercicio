df5685b0d4ad018ec218050b62c2f07e0f620558
Jeferson Machado Santos <jefersonmsantos@outlook.com>
2021-08-09 10:35:09 -0300

    adjust executa emr filename

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "executa_emr" {
-    filename = "lambda_function.payload.zip"
+    #filename = "lambda_function_payload.zip"
     function_name = var.lambda_function_name
     role = aws_iam_role.lambda.arn 
     handler = "lambda_function.handler"
